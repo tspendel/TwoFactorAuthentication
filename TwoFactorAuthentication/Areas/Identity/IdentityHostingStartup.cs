e113ebb65604fb6c5025923dca77edcaaac4abfb
Tomasz Spendel <tomaszspendel@gmail.com>
2020-01-08 21:17:25 +0100

    Scaffolded identity pages

---
+++ TwoFactorAuthentication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TwoFactorAuthentication.Areas.Identity.Data;
+
+[assembly: HostingStartup(typeof(TwoFactorAuthentication.Areas.Identity.IdentityHostingStartup))]
+namespace TwoFactorAuthentication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TwoFactorAuthenticationIdentityDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TwoFactorAuthenticationIdentityDbContextConnection")));
+
+//                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//                    .AddEntityFrameworkStores<TwoFactorAuthenticationIdentityDbContext>();
+            });
+        }
+    }
+}
